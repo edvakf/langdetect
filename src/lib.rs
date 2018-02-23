433bee576e84bc1d982cc88573802a99db941be4
Atsushi Takayama <taka.atsushi@gmail.com>
2018-02-23 23:22:14 +0900

    Minimal code that uses a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
+mod nnet_language_identifier;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
+        let lang = nnet_language_identifier::find_language("吾輩は猫である");
+        println!("{}", lang);
     }
 }
---
+++ src/nnet_language_identifier.rs
@@ -0,0 +1,4 @@
+pub fn find_language(text: &str) -> String {
+    println!("{}", text);
+    return String::from("ja");
+}
