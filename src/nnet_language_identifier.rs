e8705d81c090767f54d64cf5d6cd251fd8bf1169
Atsushi Takayama <taka.atsushi@gmail.com>
2018-02-24 01:19:42 +0900

    More implementation of NNetLanguageIdentifier::new

--- src/nnet_language_identifier.rs
+++ src/nnet_language_identifier.rs
@@ -1,15 +1,23 @@
 const UNKNOWN: &str = "und";
+const NUM_SNIPPETS: i32 = 5;
 
 pub struct NNetLanguageIdentifier {
     min_num_bytes: i32,
-    max_num_bytes: i32
+    max_num_bytes: i32,
+    num_snippets: i32,
+    snippet_size: i32
 }
 
 impl NNetLanguageIdentifier {
     pub fn new(min_num_bytes: i32, max_num_bytes: i32) -> Self {
+        let num_snippets = if max_num_bytes <= NUM_SNIPPETS { 1 } else { NUM_SNIPPETS };
+        let snippet_size = max_num_bytes / num_snippets;
+
         return Self{
             min_num_bytes: min_num_bytes,
-            max_num_bytes: max_num_bytes
+            max_num_bytes: max_num_bytes,
+            num_snippets: num_snippets,
+            snippet_size: snippet_size
         };
     }
 
